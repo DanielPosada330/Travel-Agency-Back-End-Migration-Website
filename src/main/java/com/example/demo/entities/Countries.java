78a82dfb83453522fe236f71562fb8a083f238b1
Daniel Posada <dposa11@wgu.edu>
2023-10-04 23:27:00 -0700

    Create Countries.java class in entities package: class reflects cart_items table and its respective columns in SQL database.

---
+++ src/main/java/com/example/demo/entities/Countries.java
@@ -0,0 +1,36 @@
+package com.example.demo.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+//Create Cart_item table with columns
+@Entity
+@Table(name = "countries")
+@Getter
+@Setter
+public class Countries {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //private variables represent column names in SQL table
+    @Column(name = "country_id")
+    private BigInteger country_id;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private Date create_date;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private Date last_update;
+}
