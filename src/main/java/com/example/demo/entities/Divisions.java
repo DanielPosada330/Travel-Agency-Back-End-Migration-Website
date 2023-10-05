86ed527c16983bf21fe30cc2d96c28bb370eaa5b
Daniel Posada <dposa11@wgu.edu>
2023-10-05 00:39:13 -0700

    Create Divisions.java class in entities package: class reflects cart_items table and its respective columns in SQL database.

---
+++ src/main/java/com/example/demo/entities/Divisions.java
@@ -0,0 +1,39 @@
+package com.example.demo.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+//Create Cart_item table with columns
+@Entity
+@Table(name = "divisions")
+@Getter
+@Setter
+public class Divisions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //private variables represent column names in SQL table
+    @Column(name = "division")
+    private String division;
+
+    @Column(name = "division_id")
+    private BigInteger division_id;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private Date create_date;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private Date last_update;
+
+    @Column(name = "country_id")
+    private BigInteger country_id;
+}
