ffa3ba95c5767bc6e4243afa6dec8259c5f5e4d9
Daniel Posada <dposa11@wgu.edu>
2023-10-06 18:47:06 -0700

    Update Cart_items.java, lines 39-54: Added OneToMany and ManyToOne relationships between tables and classes.

--- src/main/java/com/example/demo/entities/Cart_items.java
+++ src/main/java/com/example/demo/entities/Cart_items.java
@@ -1,18 +1,23 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigInteger;
 import java.util.Date;
+import java.util.Set;
 
 
 //Create Cart_item table with columns
 @Entity
 @Table(name = "cart_items")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class Cart_items {
@@ -31,9 +36,20 @@ public class Cart_items {
     @UpdateTimestamp
     private Date last_update;
 
-    @Column(name = "cart_id")
-    private BigInteger cart_id;
+    //@Column(name = "vacation_id")
+    //private BigInteger vacation_id;
 
-    @Column(name = "vacation_id")
-    private BigInteger vacation_id;
+    //@Column(name = "cart_id")
+    //private BigInteger cart_id;
+
+    @ManyToMany(mappedBy = "cart_items")
+    private Set<Excursions> excursions;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Carts carts;
+
+    @ManyToOne
+    @JoinColumn(name = "vacation_id", nullable = false)
+    private Vacations vacation_id;
 }
